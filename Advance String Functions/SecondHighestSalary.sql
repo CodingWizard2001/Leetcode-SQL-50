c56df619a100ee8cdd27c95aeb84dfce89af925f
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-07-24 00:54:28 +0530

    All Problems solved

---
+++ Advance String Functions/SecondHighestSalary.sql
@@ -0,0 +1,21 @@
+-- Table: Employee
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | salary      | int  |
+-- +-------------+------+
+
+-- id is the primary key (column with unique values) for this table. Each row of this table contains information about the salary of an employee.
+ 
+
+-- Write a solution to find the second highest salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+
+
+
+SELECT MAX(E1.salary) AS SecondHighestSalary
+FROM Employee E1
+JOIN Employee E2
+ON E1.salary < E2.salary;
